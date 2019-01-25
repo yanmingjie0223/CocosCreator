6cf940bc8c18576ef2947bdc751ef2794c3b177d
SNDA\yanmingjie.jack <yanmingjie.jack@shandagames.com>
2019-01-25 21:06:26 +0800

    [BASE] [ADD] 基础类添加

--- CoreProject/assets/script/core/mvc/view/BaseViewData.ts
+++
@@ -1,9 +0,0 @@
-/*
- * @Author: yanmingjie0223@qq.com
- * @Date: 2019-01-17 10:59:19
- * @Last Modified by: yanmingjie0223@qq.com
- * @Last Modified time: 2019-01-23 20:01:20
- */
-export default class BaseViewData {
-
-}
